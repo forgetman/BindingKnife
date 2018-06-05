1c26e81413f246de0de1c5c19e009f5201b410e0
yuansui <forgetman@qq.com>
2018-06-05 14:57:00 +0800

    加入RxBinding.kt

---
+++ src/main/kotlin/kotterknife/RxBinding.kt
@@ -0,0 +1,27 @@
+package kotterknife
+
+import android.view.View
+import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.Observable
+
+/**
+ * @author yuansui
+ * @since 2018/6/5
+ */
+fun View.clicks(): Observable<Any> {
+    return RxView.clicks(this)
+}
+
+fun View.clicks(action: () -> Unit) {
+    RxView.clicks(this).subscribe {
+        action()
+    }
+}
+
+fun View.longClicks(): Observable<Any> {
+    return RxView.longClicks(this)
+}
+
+fun View.longClicks(action: () -> Unit) {
+    RxView.longClicks(this).subscribe { action() }
+}
